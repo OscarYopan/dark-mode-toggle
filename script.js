7570a7db9edb2d4309bb40f69c509fe9b4b8278f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-26 15:14:17 -0300

    --

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const toggle = document.querySelector('input')
+const toggleEl = document.querySelector('input')
+const bodyEl = document.querySelector('body')
+const sun = document.querySelector('.fa-sun')
+const moon = document.querySelector('.fa-moon')
 
-toggle.addEventListener('')
+toggleEl.addEventListener('click', () => {
+  bodyEl.classList.toggle('dark')
+  sun.classList.toggle('dark')
+  moon.classList.toggle('dark')  
+})
